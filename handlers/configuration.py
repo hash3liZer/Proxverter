167648bd870ab7293c2eb38dd1c15c4055160326
hash3liZer <sheikhshameerkashif@protonmail.com>
2020-12-12 17:09:06 +0530

    Configuration Subs

--- handlers/configuration.py
+++ handlers/configuration.py
@@ -32,20 +32,89 @@ class CONFIGURATION:
         else:
             self.invalid()
 
+    def write_domain(self, _val):
+        if _val:
+            cf = configparser.ConfigParser()
+            cf.read(os.path.join(pull.BASE_DIR, 'config.ini'))
+
+            cf['configuration']['domain'] = _val[0]
+
+            fl = open(os.path.join(pull.BASE_DIR, 'config.ini'), 'w')
+            cf.write(fl)
+        else:
+            self.invalid()
+
+    def write_ipaddress(self, _val):
+        if _val:
+            cf = configparser.ConfigParser()
+            cf.read(os.path.join(pull.BASE_DIR, 'config.ini'))
+
+            cf['configuration']['ipaddress'] = _val[0]
+
+            fl = open(os.path.join(pull.BASE_DIR, 'config.ini'), 'w')
+            cf.write(fl)
+        else:
+            self.invalid()
+
+    def write_prototypes_path(self, _val):
+        if _val:
+            cf = configparser.ConfigParser()
+            cf.read(os.path.join(pull.BASE_DIR, 'config.ini'))
+
+            cf['configuration']['prototypes_path'] = _val[0]
+
+            fl = open(os.path.join(pull.BASE_DIR, 'config.ini'), 'w')
+            cf.write(fl)
+        else:
+            self.invalid()
+
+    def write_certificates_path(self, _val):
+        if _val:
+            cf = configparser.ConfigParser()
+            cf.read(os.path.join(pull.BASE_DIR, 'config.ini'))
+
+            cf['configuration']['certificates_path'] = _val[0]
+
+            fl = open(os.path.join(pull.BASE_DIR, 'config.ini'), 'w')
+            cf.write(fl)
+        else:
+            self.invalid()
+
+    def write_port(self, _val):
+        if _val:
+            cf = configparser.ConfigParser()
+            cf.read(os.path.join(pull.BASE_DIR, 'config.ini'))
+
+            cf['configuration']['port'] = _val[0]
+
+            fl = open(os.path.join(pull.BASE_DIR, 'config.ini'), 'w')
+            cf.write(fl)
+        else:
+            self.invalid()
+
+    def list_config(self):
+        cf = configparser.ConfigParser()
+        cf.read(os.path.join(pull.BASE_DIR, 'config.ini'))
+
+        keys = list(cf['configuration'].keys())
+
+        for key in keys:
+            print(key)
+
     def execute(self):
         if self.parser.subcommand == "domain":
-            return
+            self.write_domain(self.parser.args)
         elif self.parser.subcommand == "ipaddress":
-            return
+            self.write_ipaddress(self.parser.args)
         elif self.parser.subcommand == "prototypes_path":
-            return
+            self.write_prototypes_path(self.parser.args)
         elif self.parser.subcommand == "certificates_path":
-            return
+            self.write_certificates_path(self.parser.args)
         elif self.parser.subcommand == "debug":
             self.write_debug(self.parser.args)
         elif self.parser.subcommand == "port":
-            return
+            self.write_port(self.parser.args)
         elif self.parser.subcommand == "list":
-            return
+            self.list_config()
         else:
             self.invalid()
