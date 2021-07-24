212ae79b1aa30267795f5a2a00fdf965122a92c0
hash3liZer <sheikhshameerkashif@protonmail.com>
2021-07-24 23:55:34 +0500

    Added proxy configs for debian version

--- proxverter/sysprox.py
+++ proxverter/sysprox.py
@@ -1,5 +1,9 @@
 import platform
 import socket
+import fileinput
+import os
+import re
+import sys
 
 class mac_proxy:
     '''
@@ -19,11 +23,149 @@ class lin_proxy:
     '''
 
     def __init__(self, ip_address, port):
+        self.aptconf    = '/etc/apt/apt.conf'
+        self.envconf    = '/etc/environment'
+        self.bashconf   = '/etc/bash.bashrc'
+        self.wgetconf   = '/etc/wgetrc'
         self.ip_address = ip_address
-        self.port       = port
+        self.port       = str(port)
+
+        if os.geteuid() != 0:
+            raise OSError("This library requires root privileges to run")
+
+        if not os.path.isfile(self.aptconf):
+            fl = open(self.aptconf, 'w')
+            fl.close()
+
+    def set_env_vars(self):
+        exists = False
+        for line in fileinput.input(self.envconf, inplace=1):
+            if "proxy" in line:
+                exists = True
+                line = re.sub(r'(.*)_proxy=(.*)', r'\1_proxy="\1://'+self.ip_address+':'+self.port+"/\"\n", line.rstrip())
+            sys.stdout.write(line)
+
+        if not exists:
+            fl = open(self.envconf, "w")
+            fl.write("PATH=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games\"\n")
+            fl.write("http_proxy=\"http://"+self.ip_address+":"+self.port+"/\"\n")
+            fl.write("https_proxy=\"https://"+self.ip_address+":"+self.port+"/\"\n")
+            fl.write("ftp_proxy=\"ftp://"+self.ip_address+":"+self.port+"/\"\n")
+            fl.write("socks_proxy=\"socks://"+self.ip_address+":"+self.port+"/\"\n")
+            fl.close()
+
+    def set_apt_vars(self):
+        exists = False
+        for line in fileinput.input(self.aptconf, inplace=1):
+            if "Acquire::" in line and "Cache" not in line:
+                exists = True
+                line = re.sub(r'Acquire::(.*)::proxy (.*)', r'Acquire::\1::proxy "\1://'+self.ip_address+":"+self.port+"/\";\n", line.rstrip())
+            sys.stdout.write(line)
+
+        if not exists:
+            fl = open(self.aptconf, "w")
+            fl.write("Acquire::http::proxy \"http://"+self.ip_address+":"+self.port+"/\";\n")
+            fl.write("Acquire::https::proxy \"https://"+self.ip_address+":"+self.port+"/\";\n")
+            fl.write("Acquire::ftp::proxy \"ftp://"+self.ip_address+":"+self.port+"/\";\n")
+            fl.write("Acquire::http::No-Cache \"True\";\n")
+            fl.write("Acquire::socks::proxy \"socks://"+self.ip_address+":"+self.port+"/\";\n")
+            fl.close()
+
+    def set_bash_vars(self):
+        exists = False
+        for line in fileinput.input(self.bashconf, inplace=1):
+            if "export" in line:
+                exists = True
+                line = re.sub(r'export (.*)_proxy=(.*)', r'export \1_proxy=\1://'+self.ip_address+':'+self.port+'\n', line.rstrip())
+            sys.stdout.write(line)
+
+        if not exists:
+            fl = open(self.bashconf, "r+")
+            l  = fl.readlines()
+            fl.close()
+
+            fl = open(self.bashconf, "a")
+
+            if not l[-1][-1]=='\n':
+                fl.write("\n")
+
+            fl.write("export http_proxy=http://"+self.ip_address+":"+self.port+"\n")
+            fl.write("export https_proxy=https://"+self.ip_address+":"+self.port+"\n")
+            fl.write("export ftp_proxy=ftp://"+self.ip_address+":"+self.port+"\n")
+            fl.close()
+
+    def set_wget_vars(self):
+        exists = False
+        for line in fileinput.input(self.wgetconf, inplace=1):
+            if not line.startswith("#") and "proxy" in line:
+                exists = True
+                line = re.sub(r'(.*)_proxy=(.*)//(.*)', r'\1_proxy=\1://'+self.ip_address+':'+self.port+'\n', line.rstrip())
+            sys.stdout.write(line)
+
+        if not exists:
+            fl = open(self.wgetconf, "r+")
+            l  = fl.readlines()
+            fl.close()
+
+            fl = open(self.wgetconf, "a")
+            if not l[-1][-1]=='\n':
+                fl.write("\n")
+
+            fl.write("http_proxy=http://"+self.ip_address+":"+self.port+"\n")
+            fl.write("https_proxy=https://"+self.ip_address+":"+self.port+"\n")
+            fl.write("ftp_proxy=ftp://"+self.ip_address+":"+self.port+"\n")
+
+    def rem_wget_vars(self):
+        with open(self.wgetconf, "r+") as f:
+            new_f = f.readlines()
+            f.seek(0)
+            for line in new_f:
+                if "http_proxy" not in line and "https_proxy" not in line and "ftp_proxy" not in line and "socks_proxy" not in line:
+                    f.write(line)
+            f.truncate()
+            f.close()
+
+    def rem_bash_vars(self):
+        with open(self.bashconf, "r+") as f:
+            new_f = f.readlines()
+            f.seek(0)
+            for line in new_f:
+                if "export http_proxy" not in line and "export https_proxy" not in line and "export ftp_proxy" not in line and "export socks_proxy" not in line:
+                    f.write(line)
+            f.truncate()
+            f.close()
+
+    def rem_env_vars(self):
+        with open(self.envconf, "r+") as f:
+            new_f = f.readlines()
+            f.seek(0)
+            for line in new_f:
+                if "http_proxy" not in line and "https_proxy" not in line and "ftp_proxy" not in line and "socks_proxy" not in line:
+                    f.write(line)
+            f.truncate()
+            f.close()
+
+    def rem_apt_vars(self):
+        with open(self.aptconf, "r+") as f:
+            new_f = f.readlines()
+            f.seek(0)
+            for line in new_f:
+                if "Acquire::http" not in line and "Acquire::https" not in line and "Acquire::ftp" not in line and "Acquire::socks" not in line:
+                    f.write(line)
+            f.truncate()
+            f.close()
 
     def join(self):
-        pass
+        self.set_env_vars()
+        self.set_apt_vars()
+        self.set_bash_vars()
+        self.set_wget_vars()
+
+    def del_proxy(self):
+        self.rem_wget_vars()
+        self.rem_bash_vars()
+        self.rem_apt_vars()
+        self.rem_env_vars()
 
 class win_proxy:
     '''
