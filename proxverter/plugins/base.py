4c366419fda34d9be0cc05e268584b4d137f66d7
hash3liZer <sheikhshameerkashif@protonmail.com>
2021-07-29 00:31:14 +0500

    Updated test module

--- proxverter/plugins/base.py
+++ proxverter/plugins/base.py
@@ -1,3 +1,44 @@
+from proxy.http.proxy import HttpProxyBasePlugin
 
-class PluginBase:
-    pass
+## The class to be inherited
+class PluginBase(HttpProxyBasePlugin):
+
+    def name(self):
+        '''
+            Returns the name of plugin
+        '''
+        return "Plugin Base"
+
+    ## Returns the description of plugin
+    def description(self):
+        '''
+            Returns the description of plugin
+        '''
+        return "This is base proxy plugin which is to be inherited by custom coded plugins for proxverter"
+
+    def before_upstream_connection(self, request):
+        '''
+            Function to be called before the upstream connection is established.
+            Remember this is the part where the connection has not yet established.
+            And the data is still in manipulation phase.
+
+            You can edit the request here
+        '''
+        return request
+
+    def handle_client_request(self, request):
+        '''
+            Function to be called while the data is being sent to the upstream server.
+            Remember this is the part where the connection has been established.
+            And we are looking to modify on the ongoing request.
+        '''
+        return request
+
+    def handle_upstream_chunk(self, response):
+        '''
+            Response received from the server
+        '''
+        return response
+
+    def on_upstream_connection_close(self):
+        return None
--- proxverter/test/test.py
+++ proxverter/test/test.py
@@ -1,26 +1,73 @@
 import sys
 import os
 import tempfile
-import proxverter
 import logging
+import pydoc
 import multiprocessing
+import proxverter
+from proxverter.plugins import PluginBase
+
+class ProxyCheck(PluginBase):
+    def before_upstream_connection(self, request):
+        towrite = pydoc.render_doc(request, renderer=pydoc.plaintext)
+        fl = open("/tmp/filer.txt", "w")
+        fl.write(towrite)
+        fl.write("\n\n\n\n")
+        fl.write(str(request.url) + "\n")
+        fl.write(str(request.method) + "\n")
+        fl.write(str(request.code) + "\n")
+        fl.write(str(request.reason) + "\n")
+        fl.write(str(request.version) + "\n")
+        fl.write(str(request.host) + "\n")
+        fl.write(str(request.port) + "\n")
+        fl.write(str(request.path) + "\n")
+        fl.write(str(request.headers) + "\n\n")
+        fl.write(str(request.body) + "\n")
+        fl.close()
+
+        return request
+
+    def handle_client_request(self, request):
+        towrite = pydoc.render_doc(request, renderer=pydoc.plaintext)
+        fl = open("/tmp/filer2.txt", "w")
+        fl.write(towrite)
+        fl.write("\n\n\n\n")
+        fl.write(str(request.url) + "\n")
+        fl.write(str(request.method) + "\n")
+        fl.write(str(request.code) + "\n")
+        fl.write(str(request.reason) + "\n")
+        fl.write(str(request.version) + "\n")
+        fl.write(str(request.host) + "\n")
+        fl.write(str(request.port) + "\n")
+        fl.write(str(request.path) + "\n")
+        fl.write(str(request.headers) + "\n\n")
+        fl.write(str(request.body) + "\n")
+        fl.close()
+
+        return request
+
+    def handle_upstream_chunk(self, response):
+        fl = open("/tmp/filer3.txt", "w")
+        fl.write(str(response.tobytes()))
+
+        return response
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
 
     p = proxverter.Proxverter(
         "127.0.0.1",
-        8899,
+        8700,
         is_https=True,
-        verbose=True
+        verbose=True,
+        plugins=[
+            ProxyCheck
+        ]
     )
 
-    fl1 = os.path.join(tempfile.gettempdir(), 'priv.key')
-    fl2 = os.path.join(tempfile.gettempdir(), 'cert.crt')
-    fl3 = "certificate.pfx"
+    p.fetch_cert("/home/hash3lizer/Desktop/cert.crt")
+    p.fetch_pfx("/home/hash3lizer/Desktop/cert.pfx")
 
-    #p.gen_key(fl1)
-    #p.gen_cert(fl2)
-    #p.gen_pfx(fl3)
-
-    p.join(fl1, fl2)
+    p.set_sysprox()
+    p.engage()
+    p.del_sysprox()
