1756e3a25ff0794880ee7a25248b7d5a1280df03
hash3liZer <sheikhshameerkashif@protonmail.com>
2021-09-15 23:22:43 +0500

    Fixed bugs in production for linux

--- proxverter/installer.py
+++ proxverter/installer.py
@@ -1,25 +1,37 @@
 import subprocess
 import os
+import platform
 
 class Installer:
 
     def __init__(self, output=os.devnull):
         self.chocoline = 'powershell.exe "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString(\'https://community.chocolatey.org/install.ps1\'))'
         self.chocolime = 'powershell.exe "choco install {} -y"'
-        self.startupinfo = subprocess.STARTUPINFO()
-        self.startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        self.platform  = platform.system().lower()
+        if self.platform == "windows":
+            self.startupinfo = subprocess.STARTUPINFO()
+            self.startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
         self.output    = output
 
     def activate(self):
         fl = open(self.output, 'w')
-        cmline = subprocess.Popen(
-            self.chocoline,
-            shell=True,
-            stdin=subprocess.PIPE,
-            stdout=fl,
-            stderr=fl,
-            startupinfo=self.startupinfo
-        )
+        if hasattr(self, 'startupinfo'):
+            cmline = subprocess.Popen(
+                self.chocoline,
+                shell=True,
+                stdin=subprocess.PIPE,
+                stdout=fl,
+                stderr=fl,
+                startupinfo=self.startupinfo
+            )
+        else:
+            cmline = subprocess.Popen(
+                self.chocoline,
+                shell=True,
+                stdin=subprocess.PIPE,
+                stdout=fl,
+                stderr=fl
+            )
         cmline.communicate()
         fl.close()
 
